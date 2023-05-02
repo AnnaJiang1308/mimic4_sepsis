8f68aaf31914a92d278818357b219a884146a808
Xuyuan Han <xuyuan.han@outlook.com>
2023-05-02 21:09:11 +0200

    update folder name: output to output_state

--- pyws/datatransfer.py
+++ pyws/datatransfer.py
@@ -18,8 +18,9 @@ with open('itemid_label.csv', newline='') as csvfile:
         # Add the itemid to the list
         itemid_list.append(row[0])
 
-        
-conn = psycopg2.connect(host='', user='', password='', database='mimiciv')
+
+# Connect to the database, use your own username, password and database name
+conn = psycopg2.connect(host='localhost', user='postgres', password='123', database='mimiciv')
 
 
 # create table
@@ -29,12 +30,12 @@ conn = psycopg2.connect(host='', user='', password='', database='mimiciv')
 with conn.cursor() as cursor:
     
     for itemid in itemid_list:
-        command = "select stay_id, charttime, value, valuenum from sepsis_rl where itemid={} order by charttime;".format(itemid)
+        command = "select stay_id, charttime, value, valuenum from mimiciv_derived.sepsis_rl where itemid={} order by charttime;".format(itemid)
         cursor.execute(command)
             
         result = cursor.fetchall()
         df=pd.DataFrame(result)
-        df.to_csv('./output/{}.csv'.format(label[str(itemid)]),index=0)
+        df.to_csv('./output_state/{}.csv'.format(label[str(itemid)]),index=0)
         print("output:"+label[str(itemid)]+".csv")
 
 conn.close()
