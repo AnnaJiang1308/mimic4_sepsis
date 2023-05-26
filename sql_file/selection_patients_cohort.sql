f2a55acb818b17e9740a5227b1f4df1c4674c8b8
Xuyuan Han <xuyuan.han@outlook.com>
2023-05-26 13:07:04 +0200

    modified:   main.ipynb modified:   sql_file/selection_patients_cohort.sql

--- sql_file/selection_patients_cohort.sql
+++ sql_file/selection_patients_cohort.sql
@@ -8,7 +8,7 @@ create table sepsis_patients_cohort as
 	where stay_id not in (select stay_id from icustay_hourly group by stay_id having max(hr) - min(hr) < 12)
 	
 	  	  --remove patients who did not have recorded vital signs for more than 6 hours. 
-	  	  --Without removal: (subject_id, stay_id):(7871, 12874). With removal: (subject_id, stay_id):(7113, 10671)
+	  	  --Before removal: (subject_id, stay_id):(7871, 12874). After removal: (subject_id, stay_id):(7113, 10671)
 	  and stay_id not in (select stay_id from 
 	  	  (select stay_id, charttime, lag(charttime) over (partition by stay_id order by charttime) as previous_charttime from chartevents) as subquery
 	  	  where previous_charttime is not null and extract(epoch from (charttime - previous_charttime))/3600 > 6)
@@ -20,7 +20,7 @@ create table sepsis_patients_cohort as
 	  and subject_id in (select subject_id from icustays where first_careunit = 'Medical Intensive Care Unit (MICU)')
 
 	  	  --include only first icu visit per each patient 
-	  	  --Without removal: (subject_id, stay_id):(7113, 10671). With removal: (subject_id, stay_id):(6669, 6669)
+	  	  --Before removal: (subject_id, stay_id):(7113, 10671). After removal: (subject_id, stay_id):(6669, 6669)
 	  and stay_id in (select stay_id from 
 	  	  (select stay_id, row_number() over (partition by subject_id order by suspected_infection_time) as rn from sepsis3) as subquery
 		  where rn = 1)
