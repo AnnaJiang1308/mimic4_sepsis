d3ef24692b71abb806babce38674b7dde01ed09f
Xuyuan Han <xuyuan.han@outlook.com>
2023-06-08 18:29:11 +0200

    feat(action): add NaCl_0_9% & Dextrose_5% as IV fluid bolus action

--- sql_file/action_from_inputevents.sql
+++ sql_file/action_from_inputevents.sql
@@ -6,7 +6,10 @@ drop table IF EXISTS sepsis_action;
 create table mimiciv_derived.Inputeventsneeded as
 	select stay_id, itemid, starttime, endtime, amount
 	from mimiciv_icu.inputevents
-	where itemid = 220964 	--Dextrose_5%_Saline_0_9%
+	where itemid = 220964 	--Dextrose_5%_Saline_0_9%, No data
+		or itemid = 220954 	--Saline_0_9%, No data
+		or itemid = 225158 	--NaCl_0_9%, 1258660 rows
+		or itemid = 220949 	--Dextrose_5%, 1034251 rows
 		or itemid = 221906	--Norepinephrine
 		or itemid = 222315	--Vasopressin
 		or itemid = 221653	--Dobutamine
