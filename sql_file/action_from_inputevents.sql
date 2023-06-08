a6371276f0af4df6f5b7beabe1713801f3e01306
Xuyuan Han <xuyuan.han@outlook.com>
2023-06-08 17:01:36 +0200

    feat(action): add Epinephrine to itemid_label_action.csv and action.sql

--- sql_file/action_from_inputevents.sql
+++ sql_file/action_from_inputevents.sql
@@ -6,10 +6,15 @@ drop table IF EXISTS sepsis_action;
 create table mimiciv_derived.Inputeventsneeded as
 	select stay_id, itemid, starttime, endtime, amount
 	from mimiciv_icu.inputevents
-	where itemid= 220964 or itemid= 221906 or itemid= 222315 or itemid= 221653 or itemid= 221986 or itemid= 221749 or itemid=221662;
-
+	where itemid = 220964 	--Dextrose_5%_Saline_0_9%
+		or itemid = 221906	--Norepinephrine
+		or itemid = 222315	--Vasopressin
+		or itemid = 221653	--Dobutamine
+		or itemid = 221986 	--Milrinone
+		or itemid = 221749 	--Phenylephrine
+		or itemid = 221662 	--Dopamine
+		or itemid = 221289 	--Epinephrine
+		;
 create table mimiciv_derived.sepsis_action as
 	select * from mimiciv_derived.Inputeventsneeded
 	where stay_id in (select stay_id from mimiciv_derived.sepsis_patients_cohort);
-	
-	
