f0f550a8f669a27549f5b750d9ef092567a7cf90
Xuyuan Han <48040943+xuyuan-han@users.noreply.github.com>
2023-06-12 20:35:08 +0200

    feat(action in main.ipynb): obtain norepinephrine_equivalent_dose_rate (#6)
    * modified:   .gitignore
    modified:   main.ipynb
    modified:   sql_file/action_from_inputevents.sql
    modified:   sql_file/chartevents_dataneeded.sql
    new file:   sql_file/selection_patients_cohort.sql
    
    * modified:   .gitignore
    
    * Delete .vscode directory
    
    * Delete .DS_Store
    
    * fix bug in main.ipynb
    
    * modified:   main.ipynb
    modified:   sql_file/selection_patients_cohort.sql
    
    * modified:   main.ipynb
    
    * Format main.ipynb, modified:   main.ipynb
    
    * Format main.ipynb
    
    * feat(main.ipynb): make hourly sample a function and loop over stay_ids
    
    * feat(action): add Dopamine to itemid_label_action.csv and action.sql
    
    * feat(action): add Epinephrine to itemid_label_action.csv and action.sql
    
    * feat(main.ipynb): get Vasopressors Statistical Data, choose first five
    
    * refactor(main.ipynb): refactor Vasopressors Statistical Data block
    
    * refactor(main.ipynb): refactor loop through hourly_sample
    
    * feat(sql): add test.sql for testing
    
    * feat(action): add NaCl_0_9% & Dextrose_5% as IV fluid bolus action
    
    * feat(main.ipynb): add IV fluid bolus statistical data
    
    * feat(action in main.ipynb): obtain norepinephrine_equivalent_dose_rate
    
    * feat(action in main.ipynb): get count of norepinephrine_equivalent_dose
    
    * style(main.ipynb): style the whole notebook with index
    
    * feat(main.ipynb): add value_per_4hours column to action data

--- sql_file/action_from_inputevents.sql
+++ sql_file/action_from_inputevents.sql
@@ -6,10 +6,18 @@ drop table IF EXISTS sepsis_action;
 create table mimiciv_derived.Inputeventsneeded as
 	select stay_id, itemid, starttime, endtime, amount
 	from mimiciv_icu.inputevents
-	where itemid= 220964 or itemid= 221906 or itemid= 222315 or itemid= 221653 or itemid= 221986 or itemid= 221749;
-
+	where itemid = 220964 	--Dextrose_5%_Saline_0_9%, No data
+		or itemid = 220954 	--Saline_0_9%, No data
+		or itemid = 225158 	--NaCl_0_9%, 1258660 rows
+		or itemid = 220949 	--Dextrose_5%, 1034251 rows
+		or itemid = 221906	--Norepinephrine
+		or itemid = 222315	--Vasopressin
+		or itemid = 221653	--Dobutamine
+		or itemid = 221986 	--Milrinone
+		or itemid = 221749 	--Phenylephrine
+		or itemid = 221662 	--Dopamine
+		or itemid = 221289 	--Epinephrine
+		;
 create table mimiciv_derived.sepsis_action as
 	select * from mimiciv_derived.Inputeventsneeded
 	where stay_id in (select stay_id from mimiciv_derived.sepsis_patients_cohort);
-	
-	
---
+++ sql_file/test.sql
@@ -0,0 +1,78 @@
+-- set search_path to mimiciv_derived, mimiciv_hosp, mimiciv_icu, mimiciv_ed;
+-- select count(distinct subject_id), count(distinct stay_id) from mimiciv_derived.sepsis_patients_cohort;
+
+-- select * from mimiciv_icu.d_items where label like '%Dopamine%';
+-- select * from mimiciv_icu.d_items where label like '%Saline%';
+-- select * from mimiciv_icu.d_items where label like '%NaCl%';
+
+-- select * from mimiciv_icu.d_items where label like '%Dextrose%';
+-- select * from mimiciv_icu.d_items where label like '%Epinephrine%';
+
+-- select * from mimiciv_icu.d_items where category like '%Fluids%';
+
+-- select * from mimiciv_icu.d_items where itemid=220964; -- Dextrose 5% / Saline 0,9%
+-- SELECT count(*) FROM mimiciv_icu.inputevents WHERE itemid=220964; -- Dextrose 5% / Saline 0,9%: 0 rows
+
+-- select * from mimiciv_icu.d_items where itemid=220954; -- Saline 0,9%
+-- SELECT count(*) FROM mimiciv_icu.inputevents WHERE itemid=220954; -- Saline 0,9%: 0 rows
+
+-- select * from mimiciv_icu.d_items where itemid=220949; -- Dextrose 5%
+-- SELECT count(*) FROM mimiciv_icu.inputevents WHERE itemid=220949; -- Dextrose 5%: 1034251 rows
+
+-- 获取所有 Saline
+-- select count(*) from mimiciv_icu.inputevents where itemid in (select itemid from mimiciv_icu.d_items where label like '%Saline%');
+-- SELECT 
+--     i.itemid, 
+--     d.label, 
+--     count(*) as count
+-- FROM 
+--     mimiciv_icu.inputevents as i
+-- INNER JOIN 
+--     mimiciv_icu.d_items as d 
+-- ON 
+--     i.itemid = d.itemid
+-- WHERE 
+--     d.label LIKE '%Saline%'
+-- GROUP BY 
+--     i.itemid, 
+--     d.label;
+
+
+-- 获取所有 Dextrose
+-- select count(*) from mimiciv_icu.inputevents where itemid in (select itemid from mimiciv_icu.d_items where label like '%Dextrose%');
+-- SELECT 
+--     i.itemid, 
+--     d.label, 
+--     count(*) as count
+-- FROM 
+--     mimiciv_icu.inputevents as i
+-- INNER JOIN 
+--     mimiciv_icu.d_items as d 
+-- ON 
+--     i.itemid = d.itemid
+-- WHERE 
+--     d.label LIKE '%Dextrose%'
+-- GROUP BY 
+--     i.itemid, 
+--     d.label;
+
+
+-- 获取所有 NaCl
+-- select count(*) from mimiciv_icu.inputevents where itemid in (select itemid from mimiciv_icu.d_items where label like '%Dextrose%');
+-- SELECT 
+--     i.itemid, 
+--     d.label, 
+--     count(*) as count
+-- FROM 
+--     mimiciv_icu.inputevents as i
+-- INNER JOIN 
+--     mimiciv_icu.d_items as d 
+-- ON 
+--     i.itemid = d.itemid
+-- WHERE 
+--     d.label LIKE '%NaCl%'
+-- GROUP BY 
+--     i.itemid, 
+--     d.label;
+
+-- select * from mimiciv_icu.inputevents where stay_id=30000484;
