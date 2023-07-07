3d507b81d68c24ad4dc388d146376b66eadd6fd9
Hanxi Jiang <annajiang1308@gmail.com>
2023-07-07 14:21:07 +0200

    fix(hourly_sample.py): change threshold into percentage

--- python/data_preprocessing/hourly_sample.py
+++ python/data_preprocessing/hourly_sample.py
@@ -65,11 +65,7 @@ def hourly_sample_state(selected_id, itemid_list_state, label_state, k = 5):
         df_hourly=df_hourly.resample('H').asfreq(fill_value=np.nan).astype(float)
         
         df_hourly[feature]=df_hourly[feature].interpolate(method='linear', axis=0)
-        print(df_hourly[feature])
-        # df_hourly[feature]=df_hourly[feature].ffill()
-        
-   
-        
+        # df_hourly[feature]=df_hourly[feature].ffill()       
         
         df_output['chartdatetime'] = pd.to_datetime(df_output['chartdatetime'])
         #df_output = pd.concat([df_output, df_hourly], join="outer",sort=False)
